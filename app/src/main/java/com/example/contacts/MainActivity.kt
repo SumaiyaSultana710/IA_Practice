21baedaa84ae751c625a0099441295ecaaf4776c
sumaiya <sumaiya.sultana.710@gmail.com>
2022-02-07 18:58:02 +0600

    leaderboard

--- app/src/main/java/com/example/contacts/MainActivity.kt
+++ app/src/main/java/com/example/contacts/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.chip_navbar)
+        setContentView(R.layout.new_layout_figma)
     }
 }
